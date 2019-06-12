3ca74b75e6840a7941521a61b14c15acd337807b
Paulo <Paulo Alexandre>
2019-06-12 16:17:51 -0300

    first commit

---
+++ src/app/modules/cadastro/cadastro.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { runInThisContext } from 'vm';
+import { resolve } from 'url';
+import { HttpClient, HttpResponse, HttpResponseBase } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  formCadastro = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    senha: new FormControl('', [Validators.required]),
+    telefone: new FormControl('', [Validators.required, Validators.pattern('\\d{4,5}-?\\d{4}')]),
+    avatar: new FormControl('', [Validators.required], this.validaImagem.bind(this))
+  })
+
+  validarTodosOsCamposDoFormulario(form: FormGroup) {
+    Object.keys(form.controls).forEach(field => {
+      const control = form.get(field);
+      control.markAsTouched({ onlySelf: true })
+    })
+  }
+
+  validaImagem(campoDoFormulario: FormControl) {
+    return this.httpClient.head(campoDoFormulario.value,
+      {
+        observe: 'response'
+      }).pipe(map((response: HttpResponseBase) => {
+        return response.ok ? null : { urlInvalida: true }
+      }),
+        catchError((error) => {
+          return [{ urlInvalida: true }]
+        })
+      );
+  }
+
+  handleCadastrarUsuario() {
+    console.log(this.formCadastro.value);
+    if (this.formCadastro.valid) {
+      console.log(this.formCadastro.value);
+      this.formCadastro.reset();
+    } else {
+      this.validarTodosOsCamposDoFormulario(this.formCadastro);
+    }
+  }
+
+}
